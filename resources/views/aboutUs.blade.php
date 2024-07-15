edaf32588ff4b99ce4fd87fe19d036c39c5d784a
yaserhamidy <yaserhamidy1212@gmail.com>
2024-07-15 06:52:50 -0700

    first project

---
+++ resources/views/aboutUs.blade.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('bootstrap-5.3.3-dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <title>Document</title>
+
+</head>
+
+
+<body>
+    <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Blogs</a></li>
+          <li><a href="#">Contact</a></li>
+          <li class="spacer"></li>
+          <li class="logo"><p style="font-weight: 700;font-size: 2em; color: white; padding: 5px 100px;">yaser.com</p></li>
+        </ul>
+      </nav>
+     
+
+    <section class="home">
+        <div class="home-content">
+            <h3>Hello, It's Me</h3>
+            <h1>Yaser Hamidy</h1>
+            <div class="animition"><h3 style="margin: 5px;">And I'm a <h2 class="typing-effect" style="color: #03e9f4;"> web and mobile app developer</h2></h3></div>
+            <div style="width: 400px; margin: 20px;" ><p >I am a mobile app developer and web developer, skilled in creating applications for both mobile and web platforms. My expertise includes programming languages, frameworks, UI/UX design, and database management.</p></div>
+            <div class="login-box">
+
+                
+            
+                <form action="">
+                     
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        More About Me
+                    </a>
+                </form>
+            </div>
+           
+            
+            
+        </div>
+        <div class="image" style="margin: 80px;">
+            <img src="images.jpg" alt="" style="width: 300px; height: 300px; border-radius: 50%; box-shadow: 0 10px 50px #03e9f4;">
+          </div>
+          
+    </section>
+    <div><p style="margin: 0 0 100px 550px; font-size: 50px; color: white;" >My <span style="color: #03e9f4;">Survice</span></p></div>
+    <div class="one">
+        <div style="width: 400px; margin: 15px;">
+            <img src="web.jpg" alt="" class="hover-effect">
+        </div>
+        <div style="width: 400px; margin: 15px;">
+            <img src="download.jpg" alt="" class="hover-effect">
+        </div>
+        <div style="width: 400px; margin: 15px;">
+            <img src="images (1).jpg" alt="" class="hover-effect">
+        </div>
+    </div>
+    <div><p style="margin: 70px 0 100px 550px; font-size: 50px; color: white;" >My <span style="color: #03e9f4;">Skills</span></p></div>
+
+    <div class="title">
+        <div>
+            <p style="color: white;margin: 0px 0 0 190px;  font-weight: bold; font-size: 30px;">Technical Skills</p>
+        </div>
+        <div>
+            <p style="color: white;margin: 0px 0 0 370px;  font-weight: bold; font-size: 30px;">Professional Skills</p>
+        </div>
+    </div>
+   <div class="all-skills">
+
+   
+    <div class="form-container" style="text-align: left; margin: 100px;" >
+
+        
+        <form >
+
+             <label for="rangeInput">FLUTTER:</label>
+             <span id="rangeValue">70</span>%
+             <input type="range" id="rangeInput" name="rangeInput" min="0" max="100" step="1" value="70" readonly >
+             <label for="rangeInput" >HTML:</label>
+             <span id="rangeValue">90</span>%
+             <input type="range" id="rangeInput" name="rangeInput" min="0" max="100" step="1" value="90" readonly>
+ 
+             <label for="rangeInput" >Css:</label>
+             <span id="rangeValue">30</span>%
+             <input type="range" id="rangeInput" name="rangeInput" min="0" max="100" step="1" value="30" readonly>
+ 
+             <label for="rangeInput" >Java Script:</label>
+             <span id="rangeValue">10</span>%
+             <input type="range" id="rangeInput" name="rangeInput" min="0" max="100" step="1" value="10" readonly>
+         </form>
+     </div> 
+ 
+     <div class="form-container" style="text-align: left; margin: 100px;" >
+         <form >
+              <label for="rangeInput">Creativity:</label>
+              <span id="rangeValue">70</span>%
+              <input type="range" id="rangeInput" name="rangeInput" min="0" max="100" step="1" value="70" readonly >
+              <label for="rangeInput" >Problem Sloving:</label>
+              <span id="rangeValue">50</span>%
+              <input type="range" id="rangeInput" name="rangeInput" min="0" max="100" step="1" value="50" readonly>
+  
+              <label for="rangeInput" >Communication:</label>
+              <span id="rangeValue">80</span>%
+              <input type="range" id="rangeInput" name="rangeInput" min="0" max="100" step="1" value="80" readonly>
+  
+              <label for="rangeInput" >Teamwork:</label>
+              <span id="rangeValue">100</span>%
+              <input type="range" id="rangeInput" name="rangeInput" min="0" max="100" step="1" value="100" readonly>
+          </form>
+      </div> 
+   </div>
+   <div><p style="margin: 0 0 100px 550px; font-size: 50px; color: white;" >Latest <span style="color: #03e9f4;">Project</span></p></div>
+   <div class="one">
+   <div style="height: 400px; width: 300px; background-color: #040e2e; margin: 20px 50px 20px 100px; border-radius: 10px; box-shadow: 0 15px 25px rgba(0, 0, 0, .6) ;">
+    <img src="gallary1.PNG" alt="" style="padding: 20px;border-radius: 30px; width: 250px; height: 170px;  margin: 20px; margin-top: 0px; margin-bottom: 0px;">
+    <h2 style="margin: 0px 70px 10px 70px; color: #fff;"> Login page</h2>
+    <p style="text-align: justify; color: #fff; margin: 5px; margin-bottom: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius neque incidunt, omnis quod maiores recusandae.</p>
+    <p style="text-align: justify; color: #fff; margin: 5px; margin-bottom: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius neque incidunt, omnis quod maiores recusandae.</p>
+    <input class="button1" type="submit" value="Read more" style="margin: 50px; margin-top: 20px; padding: 15px; border-radius: 10px; color: #fff; background-color:#03e9f4; cursor: pointer; border: none; font-size: 30px; width: 200px; margin-right: 10px; filter: brightness(1.2);">
+</div>
+<div style="height: 400px; width: 300px; background-color: #040e2e; margin: 20px 50px 20px 100px; border-radius: 10px; box-shadow: 0 15px 25px rgba(0, 0, 0, .6) ;">
+    <img src="gallary1.PNG" alt="" style="padding: 20px;border-radius: 30px; width: 250px; height: 170px;  margin: 20px; margin-top: 0px; margin-bottom: 0px;">
+    <h2 style="margin: 0px 70px 10px 70px; color: #fff;"> Login page</h2>
+    <p style="text-align: justify; color: #fff; margin: 5px; margin-bottom: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius neque incidunt, omnis quod maiores recusandae.</p>
+    <p style="text-align: justify; color: #fff; margin: 5px; margin-bottom: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius neque incidunt, omnis quod maiores recusandae.</p>
+    <input class="button1" type="submit" value="Read more" style="margin: 50px; margin-top: 20px; padding: 15px; border-radius: 10px; color: #fff; background-color:#03e9f4; cursor: pointer; border: none; font-size: 30px; width: 200px; margin-right: 10px; filter: brightness(1.2);">
+</div>
+<div style="height: 400px; width: 300px; background-color: #040e2e; margin: 20px 50px 20px 100px; border-radius: 10px; box-shadow: 0 15px 25px rgba(0, 0, 0, .6) ;">
+    <img src="gallary1.PNG" alt="" style="padding: 20px;border-radius: 30px; width: 250px; height: 170px;  margin: 20px; margin-top: 0px; margin-bottom: 0px;">
+    <h2 style="margin: 0px 70px 10px 70px; color: #fff;"> Login page</h2>
+    <p style="text-align: justify; color: #fff; margin: 5px; margin-bottom: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius neque incidunt, omnis quod maiores recusandae.</p>
+    <p style="text-align: justify; color: #fff; margin: 5px; margin-bottom: 10px;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius neque incidunt, omnis quod maiores recusandae.</p>
+    <input class="button1" type="submit" value="Read more" style="margin: 50px; margin-top: 20px; padding: 15px; border-radius: 10px; color: #fff; background-color:#03e9f4; cursor: pointer; border: none; font-size: 30px; width: 200px; margin-right: 10px; filter: brightness(1.2);">
+</div>
+
+</div>
+
+<div class="contact1">
+
+   <div>
+    <div>
+        <div>
+            <p   style="margin: 200px 0 10px 200px; font-size: 30px; color: white;" >Contact <span style="color: #03e9f4;">Me</span></p></div>
+           </div>
+           <div>
+               <p style="color: white;font-size: 20px; margin: 10px;">Let's Work Together</p>
+           </div>
+           <div style="width: 600px; margin: 10px;">
+               <p style="color: white;font-size: 15px; ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque vel voluptatem sed voluptates voluptate inventore. Mollitia minus ducimus autem cum. Illum eius ullam temporibus optio. Accusamus modi minima quisquam corporis doloremque error unde illum. Quam vitae distinctio quas ipsa deserunt maxime cumque eos. Molestiae corrupti adipisci pariatur natus atque in repudiandae vel consequatur, quam asperiores reprehenderit maiores, iure quaerat voluptatibus explicabo impedit fuga ad ipsa facere ea suscipit! Id animi debitis labore eligendi minima veniam ut non dolorum assumenda neque ab aspernatur inventore repudiandae, nisi aliquid doloremque consequatur ad sint vel, sequi eius. Fugiat velit iure quo voluptatem, cumque dignissimos.</p>
+           </div>
+           <div class="num" style="margin: 20px ;  ">
+               <div>
+                   <p style="color: #03e9f4; font-size: 15px;">Number:</p>
+                   
+               </div>
+               <div><p style="color: white; font-size: 15px;"> 0789128280</p></div>
+           </div>
+           <div class="num" style="margin: 20px ;  ">
+               <div>
+                   <p style="color: #03e9f4; font-size: 15px;">Gmail:</p>
+                   
+               </div>
+               <div>
+                   <p style="color: white; font-size: 15px;">yaserhamidy1212@gmail.com</p>
+               </div>
+    </div>
+    
+   </div>
+ 
+
+   <div>
+    <form action=""><input type="text" placeholder="Enter Username"  style="margin-top: 200px; margin-bottom: 0px; margin-left: 50px; width: 500px; height: 40px; border-radius: 10px; ">
+        <input type="text" placeholder="Enter your LastName"  style="margin-top: 10px;margin-bottom: 0px; width: 500px; height: 40px; border-radius: 10px; margin-left: 50px; ">
+        <input type="text" placeholder="Enter your Gmail"  style="margin-top: 10px;margin-bottom: 0px; width: 500px; height: 40px; border-radius: 10px; margin-left: 50px; ">
+
+        <textarea placeholder="Enter your message" cols="68" rows="13" style=" margin-left: 50px;margin-top: 10px; border-radius: 10px;"></textarea>
+     <br>
+     <input class="button1" type="submit" style="margin: 70px; margin-top: 20px; padding: 20px; border-radius: 10px; color: #fff; background-color:#040e2e; cursor: pointer; border: none; font-size: 30px; width: 200px; margin-right: 10px; filter: brightness(1.2);">
+     <a href="follow-page.html"  class="button1"  style="margin:  50px; text-decoration: none; margin-top: 20px; padding: 20px; border-radius: 10px; color: #fff; background-color: #040e2e; border: none; cursor: pointer; font-size: 30px; width: 200px; filter: brightness(1.2); ">Follow me</a>
+    </form>
+     
+     </div>      
+       </div>
+       
+
+</body>
+</html>
